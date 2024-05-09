6712629c7caefd63dcd27ccf7c948619fe432ac3
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-09 10:15:39 -0500

    Update Introducción.py

--- pages/Introducción.py
+++ pages/Introducción.py
@@ -1,4 +1,4 @@
 
 import streamlit as st
 st.title("¡Bienvenido!")
-st.write("en esta página podrá ver el funcionamiento de varios tipos de herramientas que cumplen diferentes funciones a la vez, ya sea el uso de la voz para transformarla en texto, usar un sistema de reconicimiento facial o incluso diseñar un sistema ensamblado justo a un sistema físico y que este funcione por medio de un sistema establecido") 
+st.write("en esta página podrá ver el funcionamiento de diversos sistemas que cumplen una función similar de diversas formas, ya sea por medio de la voz, reconocimiento facial, lectura de pdf, interacción y manejo de sistemas digitales y reales juntos, entre otras cosas mas. ¡Sige explorando estos nuevos sistemas!") 

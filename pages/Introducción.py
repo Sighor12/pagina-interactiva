1682de65fe6fa738a33256373aaa4ccbc1a06c54
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-09 10:06:57 -0500

    Update Introducción.py

--- pages/Introducción.py
+++ pages/Introducción.py
@@ -1,3 +1,6 @@
 
 import streamlit as st
-st.title("¿Qué es una interface multimodal?")
+st.title("¡Bienvenido!")
+st.write("en esta página podrá ver el funcionamiento de varios tipos de herramientas que cumplen diferentes funciones a la vez, 
+ya sea el uso de la voz para transformarla en texto, usar un sistema de reconicimiento facial o incluso diseñar un sistema ensamblado justo
+a un sistema físico y que este funcione por medio de un sistema establecido") 

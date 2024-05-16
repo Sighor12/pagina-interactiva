ebb7cf70a627b215752c60c1726ba4c59215924f
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-16 11:48:39 -0500

    Update voz_a_texto.py

--- pages/voz_a_texto.py
+++ pages/voz_a_texto.py
@@ -1,1 +1,14 @@
+import os
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+from PIL import Image
+import time
+import glob
 
+from gtts import gTTS
+from googletrans import Translator
+
+st.title("Voz a texto y traductor")
+st.subheader("sistema para introducir texto por medio de voz y traducirlo")

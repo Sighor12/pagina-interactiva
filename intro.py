09a4441e40b992aa3f502adda95ef20cfac9f31b
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-16 10:10:38 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,5 +1,4 @@
 
 import streamlit as st
 st.title("Página Principal")
-
-
+st.write("bienvenido a esta página interactiva. Aqúi podras ver el uso de algunos sistemas de interacción multimodal para conocer más sobre sus usos y funciones, además de las ventajas que pueden ofrecer a largo plazo")

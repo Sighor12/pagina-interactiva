d76871257dd8319336266056be3e6e64eb6ec450
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-16 11:05:16 -0500

    Update Multimodal.py

--- pages/Multimodal.py
+++ pages/Multimodal.py
@@ -1,2 +1,5 @@
 import streamlit as st
+from PIL import Image
+
 st.title("¿Qué es una interfaz multimodal?")
+st.write("Las interfaces multimodales son sistemas de interacción múltiple que le permiten al usuario introducir algún tipo de información al sistema por medio de varios tipos de modos de entrada diferentes, ya sea por medio del ratón y teclado, habla, escritura manual, gestos, entre muchos otros tipos de entrada diferentes")

89df094f5e72e515149d4d63c2dc09dba9bf11e1
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-16 12:30:35 -0500

    Update voz_a_texto.py

--- pages/voz_a_texto.py
+++ pages/voz_a_texto.py
@@ -16,3 +16,5 @@ st.subheader("sistema para introducir texto por medio de voz y traducirlo")
 image = Image.open('voz a texto y traductor.jpg')
 
 st.image(image)
+
+st.write("en esta sección podrás interactuar con el sistema de voz al decir algún tipo de frase que quieras pasar a texto. Además, si buscas saber su significado en otro idioma, puedes decidir que tipo de salida tendrá de los distintos idiomas cargados en el sistema. Pruebalo!")

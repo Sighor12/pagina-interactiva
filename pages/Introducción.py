fa0072ffd94d290892433a0e4c21772303140394
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-16 10:24:17 -0500

    Update Introducción.py

--- pages/Introducción.py
+++ pages/Introducción.py
@@ -4,6 +4,6 @@ from PIL import Image
 st.title("¡Bienvenido!")
 st.write("en esta página podrá ver el funcionamiento de diversos sistemas que cumplen una función similar de diversas formas, ya sea por medio de la voz, reconocimiento facial, lectura de pdf, interacción y manejo de sistemas digitales y reales juntos, entre otras cosas mas. ¡Sige explorando estos nuevos sistemas!") 
 
-image = Image.open('sistema.jpg')
+image = Image.open('Sistema.jpg')
 
 st.image(image, caption='Sistema')

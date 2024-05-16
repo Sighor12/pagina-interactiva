c12acd0382b47af4cd576dc50461a266723d97f2
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-16 10:19:22 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -5,3 +5,5 @@ st.title("Página Principal")
 st.write("bienvenido a esta página interactiva. Aqúi podras ver el uso de algunos sistemas de interacción multimodal para conocer más sobre sus usos y funciones, además de las ventajas que pueden ofrecer a largo plazo")
 
 image = Image.open('saludo.jpg')
+
+st.image(image, caption='bienvenida')

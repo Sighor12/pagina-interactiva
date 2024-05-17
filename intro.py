a38fb2ef3690b58af2d64821892611363a9a39f2
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-17 16:19:38 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Página Principal")
+st.title("¡Bienvenido!")
 st.write("bienvenido a esta página interactiva. Aqúi podras ver el uso de algunos sistemas de interacción multimodal para conocer más sobre sus usos y funciones, además de las ventajas que pueden ofrecer a largo plazo")
 
 image = Image.open('saludo.jpg')

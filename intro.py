b39b0a1f99d589463da023bccaceff26499b1a9f
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-21 18:51:26 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -7,3 +7,5 @@ st.write("bienvenido a esta página interactiva. Aqúi podras ver el uso de algu
 image = Image.open('saludo.jpg')
 
 st.image(image)
+
+st.write("el objetivo de estas interfaces es para introducirte a como funcionan estos sistemas de varias formas y los diversos usos que pueden tener a largo plazo (en caso de que no hayas visto este tipo de sistemas antes)")

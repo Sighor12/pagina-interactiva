bd3bf544271bbd19e5f259d7ee47d84e741063a7
Sighor12 <160879730+Sighor12@users.noreply.github.com>
2024-05-22 21:03:14 -0500

    Update and rename Z_despedida to Z_despedida.py

--- pages/Z_despedida.py
+++ pages/Z_despedida.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 st.title("gracias por participar")
-image = Image.open('')
+image = Image.open('Despedida.jpg')
 st.image(image)
 
 st.write("muchas gracias por navegar por medio de estas interfaces, espero que hayas aprendido sobre el uso y funcionamiento de algunas interfaces multimodales que fueron diseñadas aquí. ¡Animate a descubrir más interfaces y sistemas con varias funciones y metodos de entrada! Nos vemos :D")
